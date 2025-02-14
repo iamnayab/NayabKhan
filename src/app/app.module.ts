338809388d652198c32701329e0dd4ab85284b07
iamnayab <nayabkhan0202@gmail.com>
2025-02-15 00:35:40 +0530

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import {MatExpansionModule} from  '@angular/material/expansion';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon'
-import {MatListModule} from '@angular/material/list'
+import {MatListModule} from '@angular/material/list';
+import { FooterComponent } from './footer/footer.component'
 
 
 
@@ -32,7 +33,8 @@ import {MatListModule} from '@angular/material/list'
     NotFoundComponent,
     AdminComponent,
     BlogComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
     
   ],
   imports: [
